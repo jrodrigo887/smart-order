fa04ba276b22f9566ed4a17079521257c365c3b7
jrodrigo887 <jrodrigo887@gmail.com>
2025-04-14 23:37:24 -0300

    feat: integrate Swagger for API documentation and enhance logging during application startup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
+  const configSW = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('Api v1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, configSW);
+  const logger = new Logger('Main');
+  logger.log('Swagger is starting...');
+  SwaggerModule.setup('api', app, documentFactory);
   logger.log('Nest application is starting...');
   const config = app.get(ConfigService);
   const port = config.get<number>('PORT') || 3001;
